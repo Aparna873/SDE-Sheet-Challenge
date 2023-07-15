4540a7a85aeeb9efd5bd7117fd0f509ea3354178
Aparna873 <appsworld837@gmail.com>
2023-07-15 10:31:59 +0530

    stock

---
+++ pair_sum.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+vector<vector<int>> pairSum(vector<int> &arr, int s){
+   // Write your code here.
+   sort(arr.begin(),arr.end());
+   int n=arr.size();
+   vector<vector<int>>ans;
+   for(int i=0;i<n;i++)
+   {
+     for(int j=i+1;j<n;j++)
+     {
+        int sum=arr[i]+arr[j];
+        if(sum==s)
+        {
+           ans.push_back({arr[i],arr[j]});
+        }
+     }
+   }
+   return ans;
+}
